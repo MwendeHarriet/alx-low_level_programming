d8de391af9857ef847acb545d2dd3d0687d4b85d
MwendeHarriet <mwendeharriet@gmail.com>
2023-04-27 08:34:26 -0700

    Mutiple Mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,12 +1,15 @@
 #include "main.h"
-
+/**
+ * positive or negative- tests for positive or negative
+ *
+ * @i: the number to be tested
+ *
+ * Return: Always 0
+ */
 void positive_or_negative(int i)
 {
-	if (i > 0)
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else if (i > 0)
 		printf("%d is positive\n", i);
-
-	else if (i < 0)
-		printf("%d is positive\n", i);
-	
-	else printf("%d is zero\n", i);
 }
