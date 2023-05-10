ec946ff442cbf257c9c7451460e3eb06697e8ae0
MwendeHarriet <mwendeharriet@gmail.com>
2023-05-10 09:41:55 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion- prints a string
+ *@s: string
+ * Return: string
+ */
+
+void _puts_recursion(char *s)
+{
+	*s == '\0'
+	{
+		_putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+
