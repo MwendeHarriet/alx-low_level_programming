40a812ec6cc46366e809cccf11db6959c405534f
MwendeHarriet <mwendeharriet@gmail.com>
2023-05-12 07:24:06 -0700

    args2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,11 +11,11 @@
 
 int main(int argc, char *argv[])
 {
-	int n = 0;
+	int n;
 
-	for (n < argc; n++)
+	for (n = 0; n < argc; n++)
 	{
-		printf("%s\n", argv[n])
+		printf("%s\n", argv[n]);
 	}
 	return (0);
 }
