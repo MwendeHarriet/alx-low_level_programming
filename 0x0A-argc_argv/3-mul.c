1f4a045098bb2927e551b6357f41926ad6f4d786
MwendeHarriet <mwendeharriet@gmail.com>
2023-05-12 08:48:32 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -multiplies two numbers and stores
+ * result in an integer
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	num1 = atoi(argv[1];
+	num2 = atoi(argv[2];
+	result = num1 * num2
+
+		printf("%d\n", result);
+		return (0);
+
+		else if (argv != 3)
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+}
