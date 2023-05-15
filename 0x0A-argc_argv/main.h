8900062a52518ed69211b21f0d37f22d3a6821d4
MwendeHarriet <mwendeharriet@gmail.com>
2023-05-15 00:47:09 -0700

    mul

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -6,6 +6,5 @@ int _atoi(char *s);
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[]__attribute__((unused)));
 int main(int argc, char *argv[]);
-int main(int argc, char *argv[];
 
 #endif
