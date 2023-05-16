ddd60a8af1f891b6731634bc5c52123f90f3f0e6
MwendeHarriet <mwendeharriet@gmail.com>
2023-05-16 11:21:08 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,16 +18,18 @@ char *_strdup(char *str)
 
 	int len = 0;
 	char *new_str = NULL;
+	int i;
 
 	while (str[len] != '\0')
 	len++;
 
-	new_str = malloc((len + 1) * sizeof(char));
+	new_str = (char *)malloc((len + 1) * sizeof(char));
 
 	if (new_str != NULL)
 	{
-		for (int i = 0; i <= len; i++)
+		for (i = 0; i <= len; i++)
 		new_str[i] = str[i];
 	}
+
 	return (new_str);
 }
