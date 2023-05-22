20a0c343fe29c6c152e991ee3e297278d5463bac
MwendeHarriet <mwendeharriet@gmail.com>
2023-05-22 10:45:13 -0700

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,19 +6,19 @@
 *print_dog -prints struct dog
 *description -prints struct dog
 *@d: sreuct dog to print
-*Return: on success 0
+*Return:
 */
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+		if (d == NULL)
 		return;
 
 	if (d->name == NULL)
-		d->name == "(nil)";
+		d->name = "(nil)";
 
 	if (d->owner == NULL)
-		d->owner == "(nil)";
+		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
