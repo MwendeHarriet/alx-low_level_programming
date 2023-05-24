a21f43978c9cae33734aaacaecb74a316ed4b543
MwendeHarriet <mwendeharriet@gmail.com>
2023-05-24 09:46:33 -0700

    main_opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,18 +2,34 @@
 #include <stdlib.h>
 
 /**
-* main -prints opcodes of its own main function
-*@argc: argument counter
-*@argv: argument vector
-*
-*Return: 0 on success 1 or incorrect number of arguments
-*or 2 for negative number of bytes
-*/
+ * print_opcodes - Prints the opcodes of a given function.
+ * @func: Pointer to the function.
+ * @num_bytes: Number of bytes to print.
+ */
+
+void print_opcodes(void (*func)(void), int num_bytes)
+{
+	int i;
+	unsigned char *opcodes = (unsigned char *)func;
+
+	for (i = 0; i < num_bytes; i++)
+		printf("%02x\t", opcodes[i]);
+
+	printf("\n");
+}
+
+/**
+ * main - Prints the opcodes of its own main function.
+ * @argc: The number of command line arguments.
+ * @argv: An array containing the command line arguments.
+ *
+ * Return: 0 on success, 1 for incorrect number of arguments,
+ *         and 2 for negative number of bytes.
+ */
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int n_of_bytes;
+	int num_bytes;
 
 	if (argc != 2)
 	{
@@ -21,20 +37,16 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	n_of_bytes = atoi(argv[1]);
+	num_bytes = atoi(argv[1]);
 
-	if (n_of_bytes < 0)
+	if (num_bytes < 0)
 	{
 		printf("Error\n");
 		return (2);
 	}
 
-	unsigned char *ptr = (unsigned char *)main;
-
-	for (i = 0; i < n_of_bytes; i++)
-		printf("%02x ", ptr[i]);
-
-	printf("\n");
+	print_opcodes(main, num_bytes);
 
 	return (0);
 }
+
