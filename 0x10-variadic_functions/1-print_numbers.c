b4b19e3005bfb17e8c5be08bf53d08cec86100c2
MwendeHarriet <mwendeharriet@gmail.com>
2023-05-25 08:09:05 -0700

    print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,7 +6,7 @@
 *print_numbers -prints numbers followed by \n
 *description -prints numbers followed by \n
 *@separator: string between numbers
-*n: integres passed to function
+*@n: integres passed to function
 *@...: parameters
 *Return: nothing
 */
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0, i < n; i++)
 	{
 		nums = va_args(allp, int);
-			printf("%d", va_args(allp, int);
+			printf("%d", va_args(allp, int));
 
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
