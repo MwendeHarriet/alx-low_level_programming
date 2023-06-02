6f7aec8270f653ef5a233284b8721b1b316f3f06
MwendeHarriet <mwendeharriet@gmail.com>
2023-06-02 16:22:28 -0700

    list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,18 +4,18 @@
 /**
  * list_len -calculates number of elemnts in string
  * @h: head
- * @temp: temporary pointer
+ *
  * Return: elements in h
  */
 
 size_t list_len(const list_t *h)
 {
 	size_t temp = 0;
-	
+
 	while (h)
 	{
 		temp++;
-		temp = temp->next;
+		h = h->next;
 	}
 	return (temp);
 }
