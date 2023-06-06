7f857d6aed7c173197152f155e76d1d79be524e3
MwendeHarriet <mwendeharriet@gmail.com>
2023-06-06 12:27:12 -0700

    insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	for (index = 0; temp_node != NULL && index < idx; index++)
+	for (index = 0; temp_node && index < idx; index++)
 	{
 		if (index == idx - 1)
 		{
