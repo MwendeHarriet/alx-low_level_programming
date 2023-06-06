0a733b1fd4c15eccdfe61b68cbe39d4c34a41963
MwendeHarriet <mwendeharriet@gmail.com>
2023-06-06 05:05:53 -0700

    free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list -frees list
+ * @head: head
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+		listint_t *new_node;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		new_node = (*head)->next;
+		free(*head);
+		*head = new_node;
+	}
+	*head = NULL;
+}
