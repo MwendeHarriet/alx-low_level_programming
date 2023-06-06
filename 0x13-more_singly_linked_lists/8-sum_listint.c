648e1a09a2e0f2e1f25a1c6397a0b65829e6d1d3
MwendeHarriet <mwendeharriet@gmail.com>
2023-06-06 10:27:46 -0700

    sum_listint

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint -returns sum of all the data (n)
+ * @head: head
+ * Return: sum or 0 if list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *temp_node = head;
+	int sum = 0;
+
+	if (temp_node == NULL)
+		return (0);
+	
+	while (temp_node)
+	{
+		sum += temp_node->n;
+		temp_node = temp_node->next;
+	}
+
+	return (sum);
+}
