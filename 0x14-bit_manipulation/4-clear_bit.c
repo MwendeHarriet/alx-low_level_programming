06c8d663f3e865bef2aad2f48cd196c72dc194ad
MwendeHarriet <mwendeharriet@gmail.com>
2023-06-08 12:47:34 -0700

    clear_bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,7 +4,7 @@
  * clear_bit -sets value of given bit to 0
  * @n: input
  * @index: index of bit
- * REturn: 1 if it worked or -1 if an error occurred
+ * Return: value of bit or -1 if an error occurred
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
