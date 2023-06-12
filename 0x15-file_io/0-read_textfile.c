f7349d09fba63c6a81ea5dcb3fbd1d5cfa799fd6
MwendeHarriet <mwendeharriet@gmail.com>
2023-06-12 10:42:24 -0700

    read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,14 +15,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t fildes;
 	ssize_t w;
-	ssize_t t;
+	ssize_t r;
 
 	fildes = open(filename, O_RDONLY);
 	if (fildes == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	r = read(fildes, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
 	free(buffer);
 	close(fildes);
