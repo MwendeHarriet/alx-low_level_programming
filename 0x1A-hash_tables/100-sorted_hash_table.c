2c2a91a137d2b83c47112767cf79838dd807f21d
MwendeHarriet <mwendeharriet@gmail.com>
2023-09-10 23:16:06 -0700

    the output doesnt match

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -7,7 +7,7 @@
 */
 shash_table_t *shash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	shash_table_t *ht;
 
 	if (size == 0)
 		return (NULL);
@@ -104,4 +104,95 @@ void shash_table_print(const shash_table_t *ht)
 	printf("}\n");
 }
 
+/**
+* shash_table_get - this function retrieves a value associated with a key
+* @ht: D hash_tab
+* @key: D key to look search
+* Return: D val associated with d key or NULL (!found).
+*/
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	shash_node_t *prs;
+
+	if (!ht || !key || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	prs = ht->array[index];
+	while (prs)
+	{
+		if (strcmp(prs->key, key) == 0)
+			return (prs->value);
+		prs = prs->next;
+	}
+	return (NULL);
+}
+
+#include "hash_tables.h"
+
+/**
+* free_list - frees a linked list of hash nodes
+* @head: pointer to the head
+*/
+void free_list(shash_node_t *head)
+{
+	shash_node_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->key);
+		free(tmp->value);
+		free(tmp);
+	}
+}
+
+/**
+* shash_table_delete - this function deletes hash_tab.
+* @ht: pointer to the hash _tab
+*/
+void shash_table_delete(shash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			free_list(ht->array[i]);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
+
+/**
+ * shash_table_print_rev - function to print the key:value from ht in reverse
+ * @ht: pointer to hash table
+ *
+ */
+void shash_table_print_rev(const shash_table_t *ht)
+{
+	shash_node_t *bucket;
+	int not_fin = 0;
+
+	if (!ht)
+		return;
+	bucket = ht->stail;
+	printf("{");
+	while (bucket)
+	{
+		if (not_fin)
+			printf(", ");
+		printf("'%s': '%s'", bucket->key, bucket->value);
+		not_fin = 1;
+		bucket = bucket->sprev;
+	}
+	printf("}\n");
+}
 
